30dbfdeacd2f76b5ac615c1bfeb10a02d4f03451
dialog-dimensions <dialog.dimensions@gmail.com>
2024-04-16 18:35:52 +0300

    included desk in the grid slot

--- Models/UiModels/GridSlot.cs
+++ Models/UiModels/GridSlot.cs
@@ -5,6 +5,7 @@ namespace SchedulerWeb.Models.UiModels;
 
 public class GridSlot
 {
+    public Desk Desk { get; set; }
     public DateTime StartDateTime { get; set; }
     public ExceptionType ExceptionType { get; set; }
     public Employee? Employee { get; set; }
